f823c1a18864646acddc57387f3da9431b8f5a52
carlospenaforte <carlosedu_1132@hotmail.com>
2025-04-12 13:08:40 -0300

    Created Folder Data to get the context. Create In Models Page the files for the datas dish, dishingredient and ingredient

---
+++ MenuProject/Models/DishIngredient.cs
@@ -0,0 +1,12 @@
+﻿namespace MenuProject.Models
+{
+    public class DishIngredient
+    {
+        public int Id { get; set; }
+        public Dish DishId { get; set; }
+        public int IngredientId { get; set; }
+
+        public Dish Dish { get; set; }
+        public Ingredient Ingredient { get; set; }
+    }
+}
